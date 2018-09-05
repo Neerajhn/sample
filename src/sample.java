5ce8084f084853745a098e262052636967bf3e7d
kavya <kavyam@thoughtworks.com>
2018-09-06 00:01:11 +0530

    create new project

---
+++ src/sample.java
@@ -0,0 +1,7 @@
+public class sample {
+  public static void main(String[] args) {
+    System.out.println("Create a project in gitlab");
+    System.out.println("copy the clone link");
+    System.out.println("git clone ctrl+v");
+  }
+}

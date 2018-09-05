30f969f7ca54c33bb4a4c00ae72e0de5a8758185
NeerajHoskote <neerajhn94@gmail.com>
2018-09-06 01:10:53 +0530

    second commit

--- src/sample.java
+++ src/sample.java
@@ -1,7 +1,7 @@
 public class sample {
   public static void main(String[] args) {
     System.out.println("Create a project in gitlab");
-    System.out.println("copy the clone link");
+    System.out.println("copy the clone link after changing it to ssh");
     System.out.println("git clone ctrl+v");
   }
 }
